5a44c0ccb5501f6c99214b89fd4bb361c73be21a
Artem Sheptunov <106321977+Infindery@users.noreply.github.com>
2024-09-24 15:57:05 -0700

    chore: update database port

--- config/database.ts
+++ config/database.ts
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
+      port: env.int('DATABASE_PORT', 30125),
       database: env('DATABASE_NAME', 'pelican_db'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', 'admin'),
