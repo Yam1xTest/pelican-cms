a6655492435bac4f2f5103f8946ab0126ba1616f
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-10-01 14:21:29 +0500

    ci:  update ci config

--- config/admin.ts
+++ config/admin.ts
@@ -1,13 +1,16 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    // secret: env('ADMIN_JWT_SECRET'),
+    secret: env('xgb5x7cdhbCNbveTtVtq2Q=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    // salt: env('API_TOKEN_SALT'),
+    salt: env('hR/XPb6VISQZg71UPYijWg=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      // salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('3pCV+b5ISok+iqUiZtTpQQ=='),
     },
   },
   flags: {
