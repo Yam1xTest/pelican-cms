bb3067ba78c5394841e49e4d3a81121f2744fd1d
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-10-01 10:11:48 +0500

    ci: update database path

--- config/database.ts
+++ config/database.ts
@@ -2,7 +2,8 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '10.244.0.7'),
+      // host: env('DATABASE_HOST', '10.244.0.7'),
+      host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'pelican_db'),
       user: env('DATABASE_USERNAME', 'postgres'),
