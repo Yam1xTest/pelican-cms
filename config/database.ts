11fccb6b94b4a49d0041819f2b63d0df94d36fb2
Ekaterina <echekina@gmail.com>
2024-10-02 11:09:13 +0500

    ci:  update database host

--- config/database.ts
+++ config/database.ts
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '10.244.0.8'),
+      host: env('DATABASE_HOST', 'postgres-hl'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'pelican_db'),
       user: env('DATABASE_USERNAME', 'postgres'),
