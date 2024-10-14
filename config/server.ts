de447429278de6c43c48b7a1b4af18d02c281864
Anna Kolesnikova <ankolesnikova@tourmalinecore.com>
2024-10-14 10:50:44 +0500

    chore: change server url

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('SERVER_URL', 'http://localhost:1337'),
+  url: 'http://pelican.local.tourmalinecore.internal/cms',
   app: {
     keys: env.array('APP_KEYS'),
     // keys: "l447ZbufnsvHicHvfpMoJA==,b00dkarNr56Er5mJLMr38w==,+GhweEM/mZi8NbBpkZIpDw==,TNAQ7URlV9PVGg7+zFXr7w==",
