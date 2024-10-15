8884094b000a736b5441c6cfcececa3a3e889a87
Anna Kolesnikova <ankolesnikova@tourmalinecore.com>
2024-10-15 14:41:03 +0500

    chore: try to firstly get variable and use it after it

--- config/server.ts
+++ config/server.ts
@@ -1,14 +1,11 @@
 export default ({ env }) => {
   const serverUrl = env('SERVER_URL', 'http://localhost:1337');
+  console.log('Server url is: ');
   console.log(serverUrl);
-  if (!serverUrl) {
-    console.log('SERVER_URL is not set or is empty');
-  }
-
   return {
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
-    url: serverUrl,
+    url: env('SERVER_URL', 'http://localhost:1337'),
     app: {
       keys: env.array('APP_KEYS'),
       // keys: "l447ZbufnsvHicHvfpMoJA==,b00dkarNr56Er5mJLMr38w==,+GhweEM/mZi8NbBpkZIpDw==,TNAQ7URlV9PVGg7+zFXr7w==",
