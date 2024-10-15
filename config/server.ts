3412414059138352fc4f6da3a69d17c0b23ea117
Anna Kolesnikova <ankolesnikova@tourmalinecore.com>
2024-10-15 11:38:50 +0500

    chore: add debug url

--- config/server.ts
+++ config/server.ts
@@ -1,12 +1,13 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url:  env('SERVER_URL'), 
-  app: {
-    keys: env.array('APP_KEYS'),
-    // keys: "l447ZbufnsvHicHvfpMoJA==,b00dkarNr56Er5mJLMr38w==,+GhweEM/mZi8NbBpkZIpDw==,TNAQ7URlV9PVGg7+zFXr7w==",
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-});
+export default ({ env }) => {
+  const serverUrl = env('SERVER_URL', 'http://localhost:1337');
+
+  if (!serverUrl) {
+    throw new Error('SERVER_URL is not set or is empty');
+  }
+
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    url: serverUrl,
+  };
+};
