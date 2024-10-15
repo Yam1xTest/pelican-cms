831d87a425202706fc0f739ded966d2226ccef05
Anna Kolesnikova <ankolesnikova@tourmalinecore.com>
2024-10-15 14:19:11 +0500

    chore: add debug url

--- config/server.ts
+++ config/server.ts
@@ -8,7 +8,7 @@ export default ({ env }) => {
   return {
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
-    url: 'http://pelican.local.tourmalinecore.internal/cms',
+    url: env(serverUrl, 'http://localhost:1337'),
     app: {
       keys: env.array('APP_KEYS'),
       // keys: "l447ZbufnsvHicHvfpMoJA==,b00dkarNr56Er5mJLMr38w==,+GhweEM/mZi8NbBpkZIpDw==,TNAQ7URlV9PVGg7+zFXr7w==",
