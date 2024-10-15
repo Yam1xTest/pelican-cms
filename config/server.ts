ae0e84d391023f17e730223e2efefea16174f93c
Anna Kolesnikova <ankolesnikova@tourmalinecore.com>
2024-10-15 15:41:23 +0500

    chore: debug

--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,13 @@
 export default ({ env }) => {
+  const serverUrl = env('SERVER_URL', 'http://localhost:1337');
+  console.log('Server URL:', serverUrl);
+
   return {
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
-    url: env('SERVER_URL', 'SERVER_URL'),
+    url: serverUrl,
     app: {
       keys: env.array('APP_KEYS'),
-      // keys: "l447ZbufnsvHicHvfpMoJA==,b00dkarNr56Er5mJLMr38w==,+GhweEM/mZi8NbBpkZIpDw==,TNAQ7URlV9PVGg7+zFXr7w==",
     },
     webhooks: {
       populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
