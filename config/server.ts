ff30a44ad36519c987f4a366525479db7a0ed75d
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2024-10-16 14:48:48 +0500

    ci: change config

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('SERVER_URL', 'http://127.0.0.1:1337'),
+  url: env('SERVER_URL'),
   proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
