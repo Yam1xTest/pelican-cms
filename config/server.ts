11b97af1487632e9fb6c086a5505f82b2b3317c0
Anna Kolesnikova <ankolesnikova@tourmalinecore.com>
2024-10-16 11:51:44 +0500

    chore: debug env

--- config/server.ts
+++ config/server.ts
@@ -1,26 +1,30 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  emitErrors: false,
-  url: env('PUBLIC_URL', 'http://localhost:1337'),
-  proxy: env.bool('IS_PROXIED', true),
-  cron: {
-    enabled: env.bool('CRON_ENABLED', false),
-  },
-  transfer: {
-    remote: {
-      enabled: false,
+export default ({ env }) => {
+  console.log('Strapi URL:', env('PUBLIC_URL'));
+
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    app: {
+      keys: env.array('APP_KEYS'),
     },
-  },
-  logger: {
-    updates: {
-      enabled: false,
+    emitErrors: false,
+    url: env('PUBLIC_URL', 'http://localhost:1337'),
+    proxy: env.bool('IS_PROXIED', true),
+    cron: {
+      enabled: env.bool('CRON_ENABLED', false),
     },
-    startup: {
-      enabled: false,
+    transfer: {
+      remote: {
+        enabled: false,
+      },
     },
-  },
-});
+    logger: {
+      updates: {
+        enabled: false,
+      },
+      startup: {
+        enabled: false,
+      },
+    },
+  };
+};
