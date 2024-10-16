dee4a78a8aa7ec5f48bfc2833f8eafd4ed9ca70a
Anna Kolesnikova <ankolesnikova@tourmalinecore.com>
2024-10-16 15:01:59 +0500

    chore: add server_url to dockerfile arg

--- config/server.ts
+++ config/server.ts
@@ -4,17 +4,12 @@ export default ({ env }) => {
   return {
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
+    url: env('SERVER_URL', 'http://localhost:1337'),
     app: {
       keys: env.array('APP_KEYS'),
     },
     emitErrors: false,
-    url: env('PUBLIC_URL', 'http://localhost:1337'),
     proxy: env.bool('IS_PROXIED', true),
-    admin: {
-      url: '/cms/admin',
-      serveAdminPanel: true,
-      path: '/cms',
-    },
     cron: {
       enabled: env.bool('CRON_ENABLED', false),
     },
