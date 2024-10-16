98dcf3f8e53d1bb48f1a3486f717803ccbd0f2d2
Anna Kolesnikova <ankolesnikova@tourmalinecore.com>
2024-10-16 15:19:25 +0500

    chore: update config

--- config/server.ts
+++ config/server.ts
@@ -1,30 +1,13 @@
-export default ({ env }) => {
-  console.log('Strapi URL:', env('PUBLIC_URL'));
 
-  return {
-    host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
-    url: env('SERVER_URL', 'http://localhost:1337'),
-    app: {
-      keys: env.array('APP_KEYS'),
-    },
-    emitErrors: false,
-    proxy: env.bool('IS_PROXIED', true),
-    cron: {
-      enabled: env.bool('CRON_ENABLED', false),
-    },
-    transfer: {
-      remote: {
-        enabled: false,
-      },
-    },
-    logger: {
-      updates: {
-        enabled: false,
-      },
-      startup: {
-        enabled: false,
-      },
-    },
-  };
-};
+export default ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: env('SERVER_URL', 'http://127.0.0.1:1337'),
+  proxy: true,
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+});
