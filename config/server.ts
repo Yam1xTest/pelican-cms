405d2c56d975b94b4d112269e05f0f02982805cc
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2024-10-16 10:28:55 +0500

    chore: change config

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,7 @@ export default ({ env }) => {
   return {
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
+    url: env('SERVER_URL', 'http://localhost:1337'),
     app: {
       keys: env.array('APP_KEYS'),
     },
