a4cfa4e37dde82c65aa80ab1f86521d75dc1627d
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2024-10-16 14:24:32 +0500

    ci: change config

--- config/server.ts
+++ config/server.ts
@@ -1,14 +1,12 @@
-export default ({ env }) => {
-  return {
-    host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
-    url: env('SERVER_URL', 'http://127.0.0.1:1337'),
-    proxy: true,
-    app: {
-      keys: env.array('APP_KEYS'),
-    },
-    webhooks: {
-      populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-    },
-  };
-};
+export default ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: env('SERVER_URL', 'http://127.0.0.1:1337'),
+  proxy: true,
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+});
