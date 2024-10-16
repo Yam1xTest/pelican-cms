947b8c71bc176f4cb64f17ee55f6f4be2e321c84
Anna Kolesnikova <ankolesnikova@tourmalinecore.com>
2024-10-16 10:16:41 +0500

    chore: remove debug

--- config/server.ts
+++ config/server.ts
@@ -1,17 +1,7 @@
 export default ({ env }) => {
-  const environment = env('ENVIRONMENT', 'local-run');
-
-  const serverUrl = environment === 'local-env'
-    ? 'http://pelican.local.tourmalinecore.internal/cms'
-    : 'http://localhost:1337';
-
-  console.log('Environment:', environment);
-  console.log('Server URL:', serverUrl);
-
   return {
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
-    url: serverUrl,
     app: {
       keys: env.array('APP_KEYS'),
     },
