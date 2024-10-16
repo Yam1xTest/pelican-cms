359f0da5ace2f982bd50e8af5467f72590f00a46
Anna Kolesnikova <ankolesnikova@tourmalinecore.com>
2024-10-16 10:34:08 +0500

    chore: add full configuration from docs

--- config/server.ts
+++ config/server.ts
@@ -1,12 +1,26 @@
-export default ({ env }) => {
-  return {
-    host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
-    app: {
-      keys: env.array('APP_KEYS'),
+export default ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  emitErrors: false,
+  url: env('PUBLIC_URL', 'http://localhost:1337'),
+  proxy: env.bool('IS_PROXIED', true),
+  cron: {
+    enabled: env.bool('CRON_ENABLED', false),
+  },
+  transfer: {
+    remote: {
+      enabled: false,
     },
-    webhooks: {
-      populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  logger: {
+    updates: {
+      enabled: false,
     },
-  };
-};
+    startup: {
+      enabled: false,
+    },
+  },
+});
